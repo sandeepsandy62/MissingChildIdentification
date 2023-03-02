0fc9e1208ca83b798f1f4a8730fcc643405754d9
sandeepsandy62 <sandeepgogarla649@gmail.com>
2023-03-02 23:53:19 +0530

    user login and register implemented in backend

--- server/DB/userModel.js
+++ server/DB/userModel.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:[true,"Please provide an FirstName"],
+        unique:false
+    },
+    lastName:{
+        type:String,
+        unique:false
+    },
     email:{
         type:String,
         required:[true,"Please provide an Email"],
@@ -11,6 +20,37 @@ const UserSchema = new mongoose.Schema({
         required:[true,"Please provide a password"],
         unique:false,
     },
+    mobileNumber:{
+        type:String,
+        required:[true,"Please provide mobile number"],
+        unique:true,
+    },
+    aadharNumber:{
+        type:String,
+        required:[true,"Please provide aadhar number"],
+        unique:true,
+    },
+    address:{
+        type:String,
+        required:[true,"Please provide address"],
+        unique:false,
+    },
+    pincode:{
+        type:String,
+        required:[true,"Please provide pincode"],
+        unique:false,
+    },
+    district:{
+        type:String,
+        required:[true,"Please provide district"],
+        unique:false,
+    },
+    state:{
+        type:String,
+        required:[true,"Please provide state"],
+        unique:false,
+    }
+
 })
 
 module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
--- server/app.js
+++ server/app.js
@@ -2,23 +2,47 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const auth = require("./auth");
 
 // body parser configuration
 app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (request, response, next) => {
-  response.json({ message: "Hey! This is your server response!" });
-  next();
-});
-
+app.use(bodyParser.urlencoded({ extended: true }));
 //require the database connection
 const dbConnect = require("./DB/dbConnect");
 
 //execute the database connection
 dbConnect();
 
+//Curb cores Error by adding a header here
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin,X-Requested-With,Content,Accept,Content-Type,Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,POST,PUT,DELETE,PATCH,OPTIONS"
+    );
+    next();
+})
+
 const User = require("./db/userModel");
+
+//Routes
+
+//Home Route
+app.get("/", (request, response, next) => {
+  response.json({ message: "Hey! This is your server response!" });
+  next();
+});
+
+
+//authentication endpoint
+app.get("/auth-endpoint",auth,(request,response)=>{
+    response.json({message:"You are authorized to access me"});
+});
 
 //register endpoint
 app.post("/register",(request,response) => {
@@ -27,8 +51,16 @@ app.post("/register",(request,response) => {
     .then((hashedPassword)=>{
         //create a new user instance and collect the data
         const user = new User({
+            firstName : request.body.firstName,
+            lastName : request.body.lastName,
             email : request.body.email,
             password:hashedPassword,
+            mobileNumber:request.body.mobileNumber,
+            aadharNumber : request.body.aadharNumber,
+            address : request.body.aadharNumber,
+            pincode : request.body.pincode,
+            district : request.body.district,
+            state : request.body.state
         });
 
         //save the new user
@@ -56,5 +88,70 @@ app.post("/register",(request,response) => {
         });
     });
 });
+
+//login endpoint
+app.post("/login",(request,response)=>{
+    //check if email exists
+    User.findOne({email:request.body.email})
+
+    //if email exists
+    .then((user)=>{
+        //compare the password entered and the hashed password found
+        bcrypt.compare(request.body.password,user.password)
+        //if the passwords match
+        .then((passwordCheck)=>{
+            //check if password matches
+            if(!passwordCheck){
+                return response.status(400).send({
+                    message:"Passwords does not match",
+                    error,
+                });
+            }
+
+            //create JWT token
+            const token = jwt.sign(
+                {
+                    userId:user._id,
+                    userEmail:user.email,
+                },
+                "RANDOM-TOKEN",
+                {expiresIn: "24h"}
+            );
+
+            //return success response
+            response.status(200).send({
+                message:"Login Successful",
+                emial:user.email,
+                token,
+            })
+        })
+        //catch error if password does not match
+        .catch((error)=>{
+            response.status(400).send({
+                message:"Passwords does not match",
+                error,
+            });
+        })
+    })
+    //catch error if email does not exist
+    .catch((e) => {
+        response.status(404).send({
+            message:"Email not found",
+            e,
+        });
+    });
+})
+
+//free endpoint
+app.get("/free-endpoint",(request,response)=>{
+    response.json({message:"You are free to access me anytime"});
+});
+
+//authentication endpoint
+app.get("/auth-endpoint",(request,response)=>{
+    response.json({message:"You are authorized to access me"});
+});
+
+
 
 module.exports = app;
