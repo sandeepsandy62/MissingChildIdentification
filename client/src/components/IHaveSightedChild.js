74223388fd20917f10d5ecf4938db758f0a94b8f
sandeepsandy62 <sandeepgogarla649@gmail.com>
2023-03-25 20:27:48 +0530

    implemented whole project functionality without age progression

--- client/src/components/IHaveSightedChild.js
+++ client/src/components/IHaveSightedChild.js
@@ -176,7 +176,7 @@ Team Child Chaser`;
                     "http://localhost:8000/send-email",
                     {
                       sender_email_id: "newbie021122@gmail.com",
-                      sender_email_id_password: "ssjbtjqamsxmtxdl",
+                      sender_email_id_password: "gmail apps password",
                       receiver_email_id: fatherEmail,
                       message: message,
                     },{
